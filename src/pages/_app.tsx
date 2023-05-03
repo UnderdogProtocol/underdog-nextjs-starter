c78809f51001ede155d4fe309cf9ff2fa2033ce8
Kevin Arifin <kevarifin14@gmail.com>
2023-05-03 11:43:23 -0700

    move ordering for styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Providers } from "@/components/Providers";
+import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
