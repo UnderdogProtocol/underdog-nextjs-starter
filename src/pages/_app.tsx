4ce598e8720e64a6539bc24cc258bddc54b57334
Kevin Arifin <kevarifin14@gmail.com>
2023-05-03 11:46:12 -0700

    add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,15 @@ import { Providers } from "@/components/Providers";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Providers>
+      <Head>
+        <title>Starter | Underdog Protocol</title>
+      </Head>
+
       <Component {...pageProps} />
     </Providers>
   );
